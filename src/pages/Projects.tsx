cb1ddb4ee16303cbc0cdea045fb2b7977cd772ae
adam sirota <adamsirota2006@gmail.com>
2024-10-05 18:22:30 -0400

    projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import './Projects.css'; // Assuming you have a CSS file to style the page
 
 const Projects: React.FC = () => {
     return (

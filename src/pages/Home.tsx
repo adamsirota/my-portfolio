fc6e25b916c73bacfa7889b9b2f4235be02f3e4b
adam sirota <adamsirota2006@gmail.com>
2024-10-05 18:45:34 -0400

    nnn

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
             <Header />
             <section id="about">
                 <h2>Introduction</h2>
-                <p>Hey, I'm Adam Sirota. I'm from New York City and currently studying Computer Science at the University of Maryland.</p>
+                <p>Hey, I'm Adam Sirota. I'm from New York City and am currently studying Computer Science at the University of Maryland.</p>
                 <p>Feel free to contact me at <a href="mailto:adamsirota2006@gmail.com">adamsirota2006@gmail.com</a> or <a href="mailto:asirota2@umd.edu">asirota2@umd.edu</a></p>
             </section>
             <Footer />

1e113757393349442067c087c65f0c24549cc20e
adam sirota <adamsirota2006@gmail.com>
2024-11-06 14:01:14 -0500

    project cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,39 +30,50 @@ const Home: React.FC = () => {
                 <p>VS Code, Eclipse, Git, Jupyter Notebook, JUnit Testing</p>
             </section>
             <section id="projects">
-                <h1>Projects</h1>
-                
-                <article>
-                    <a className = "proj" href="https://github.com/adamsirota/bank-complaints-analysis" target='_blank'>Bank Complaint Analysis</a>
+                <h1>Projects</h1> 
+                <div className="project-card">
+                    <h3 className="project-title">
+                    <a href="https://github.com/adamsirota/bank-complaints-analysis" target='_blank'>
+                        Bank Complaint Analysis <i className="fab fa-github"></i>
+                    </a>
+                    </h3>
                     <p>Capstone project made during pre-internship program at Citigroup. Used data science, machine learning, and artificial intelligence techniques to:</p>
                     <ol>
-                        <li>Find and visualize trends in a large dataset of complaints filed to banks.</li>
-                        <li>Train a model to classify new incoming complaints automatically.</li>
+                    <li>Find and visualize trends in a large dataset of complaints filed to banks.</li>
+                    <li>Train a model to classify new incoming complaints automatically.</li>
                     </ol>
                     <p>Presented findings to Citigroup executives and peers.</p>
-                </article>
-
-                <article>
-                    <a className = "proj" href="https://github.com/adamsirota/pitch-similarity-app" target='_blank'>Pitch Similarity App</a>
+                </div>
+                <div className="project-card">
+                    <h3 className="project-title">
+                    <a href="https://github.com/adamsirota/pitch-similarity-app" target='_blank'>
+                        Pitch Similarity App <i className="fab fa-github"></i>
+                    </a>
+                    </h3>
                     <p>Flask app that takes user input of attributes of a baseball pitch and finds the most similar pitches using up-to-date data from PyBaseball.</p>
-                </article>
-
-                <article>
-                    <a className = "proj" href="https://github.com/adamsirota/grade-calculator-app" target='_blank'>Grade Calculator App</a>
+                </div>
+                <div className="project-card">
+                    <h3 className="project-title">
+                    <a href="https://github.com/adamsirota/grade-calculator-app" target='_blank'>
+                        Grade Calculator App <i className="fab fa-github"></i>
+                    </a>
+                    </h3>
                     <p>Flutter app made in app development class. Acts as a normal calculator and a GPA calculator that:</p>
                     <ul>
-                        <li>Takes user input for a student's classes and grades.</li>
-                        <li>Calculates average and converts to 4.0-scale GPA.</li>
-                        <li>Can calculate the total points the student needs to reach a target GPA/average.</li>
+                    <li>Takes user input for a student's classes and grades.</li>
+                    <li>Calculates average and converts to 4.0-scale GPA.</li>
+                    <li>Can calculate the total points the student needs to reach a target GPA/average.</li>
                     </ul>
-                </article>
-
-                <article>
-                    <a className = "proj" href="https://github.com/adamsirota/ai-song-recommender" target='_blank'>AI Song Recommender</a>
+                </div>
+                <div className="project-card">
+                    <h3 className="project-title">
+                    <a href="https://github.com/adamsirota/ai-song-recommender" target='_blank'>
+                        AI Song Recommender <i className="fab fa-github"></i>
+                    </a>
+                    </h3>
                     <p>Project made in preparation for Citigroup pre-internship program. Uses cosine similarity to recommend similar songs from a dataset based on user input.</p>
-                </article>
-                
-            </section>
+                </div>
+                </section>
             <Footer />
         </div>
     );
